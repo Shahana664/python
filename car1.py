9308f3c4bd8b175945b8389fcf444c4e20f80ed3
Shahana664 <pgmsk0106@gmail.com>
2025-12-25 18:51:11 +0530

    Add files via upload

---
+++ car1.py
@@ -0,0 +1,26 @@
+class Car:
+    
+    
+    def __init__(self):
+        self.name="thar"
+        self.price=234567
+        self.colour="blue"
+        
+        def accelerate(self):
+            print("iam accelerating")
+            print(self.colour)
+
+
+        def break_system(self):
+            print("car is stopping")
+
+
+            def start(self):
+                print("car is starting")
+
+                C1=car()
+
+                C1.start()
+                C1.accelerate()
+                C1=break_system()
+            
