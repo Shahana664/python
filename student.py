a83c6b127e5f9d375518a74e623818d93edab1c4
Shahana664 <pgmsk0106@gmail.com>
2025-12-25 22:55:55 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self):
+        self.name="ram"
+        self.year=2025
+        self.branch="aiml"
+        def study(self):
+            print("i am sleeping")
+            print(self.branch)
+            s1=Student()
+            print(s1.name)
+            s1.study()
